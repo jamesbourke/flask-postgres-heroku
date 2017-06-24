5dc1c2fd496ecf45fbd6c5f29abf494345f30c61
James <james.bourke@tictrac.com>
2017-06-24 11:24:28 +0100

    mod requirements

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.heroku import Heroku
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/pre-registration'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///pre-registration'
 heroku = Heroku(app)
 db = SQLAlchemy(app)
 
